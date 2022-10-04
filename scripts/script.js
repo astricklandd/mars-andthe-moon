3b6a5e6d4fd8c77c9f8c94c249055b30fc1c87e9
astricklandd <111793985+astricklandd@users.noreply.github.com>
2022-10-04 14:37:12 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
+$(document).ready(function(){
+      $("#NameGame").click(function() {
+        onclick=location.href='https://astricklandd.github.io/delta-lab/';
+      })
 
+      $("#DogeGame").click(function() {
+        onclick=location.href='https://astricklandd.github.io/cis376-charlie-lab-1/';
+      })
+  });
